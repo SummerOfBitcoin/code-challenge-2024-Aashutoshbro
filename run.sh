50d570be401bfb40d3037bf3db1562ef31505b32
Aashutoshbro <aashutoshbro1@gmail.com>
2024-04-15 17:48:15 +0545

    try hunxa ki

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 # Update this file to run your own code
-node demo.js
+# node demo.js
 
 # python3 main.py
+
+
+# Update this file to run your own code
+node demo.js > output.txt
+
+sed -i '' -e '${/^$/d;}' output.txt
+
+echo "Script execution completed. Output saved in output.txt"
